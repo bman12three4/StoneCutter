58f8bac0205d6e645c4aa5c783c649abf0b54a35
bman12three4 <byron.lathi@student.lovett.org>
2016-10-30 12:09:26 -0400

    Add Javadoc to Date

--- src/lib/util/Date.java
+++ src/lib/util/Date.java
@@ -1,29 +1,60 @@
 package lib.util;
 
+/**
+ * A Class for using and storing dates.
+ * @author Byron Lathi
+ *
+ */
 public class Date {
 	int day, month, year;
+	/**
+	 * Creates a date object in Day Month Year format. 
+	 * @param day The day of the month
+	 * @param month The number of the month
+	 * @param year The year
+	 */
 	public Date(int day, int month, int year){
 		this.day = day;
 		this.month = month;
 		this.year = year;
 	}
 	
+	/**
+	 * Get the day
+	 * @return The day
+	 */
 	public int getDay(){
 		return day;
 	}
 	
+	/**
+	 * Get the month
+	 * @return The month in number format
+	 */
 	public int getMonth(){
 		return month;
 	}
 	
+	/**
+	 * Get all the digits of the year, usually 4.
+	 * @return The full year
+	 */
 	public int getYear(){
 		return year;
 	}
 	
+	/**
+	 * Get just the last two digits of the year
+	 * @return The last two digits of the year
+	 */
 	public int getYearDigits(){
 		return year % 100;
 	}
 	
+	/**
+	 * Get the name of the month
+	 * @return the name of the month
+	 */
 	public String getMonthWord(){
 		switch (month){
 		case 1:
