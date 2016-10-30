1e7ae9c4e49153e440496cb15e0c7c7c945fa20c
im <byron.lathi@student.lovett.org>
2016-10-30 03:15:12 -0400

    Added Date.java
    Useful for keeping dates and being able to convert them between
    different forms.

--- src/lib/util/Date.java
+++ src/lib/util/Date.java
@@ -1,5 +1,57 @@
 package lib.util;
 
 public class Date {
-
+	int day, month, year;
+	public Date(int day, int month, int year){
+		this.day = day;
+		this.month = month;
+		this.year = year;
+	}
+	
+	public int getDay(){
+		return day;
+	}
+	
+	public int getMonth(){
+		return month;
+	}
+	
+	public int getYear(){
+		return year;
+	}
+	
+	public int getYearDigits(){
+		return year % 100;
+	}
+	
+	public String getMonthWord(){
+		switch (month){
+		case 1:
+			return "January";
+		case 2:
+			return "February";
+		case 3:
+			return "March";
+		case 4:
+			return "April";
+		case 5:
+			return "May";
+		case 6:
+			return "June";
+		case 7:
+			return "July";
+		case 8:
+			return "August";
+		case 9:
+			return "September";
+		case 10:
+			return "October";
+		case 11:
+			return "November";
+		case 12:
+			return "December";
+		default:
+			return null;
+		}
+	}
 }
